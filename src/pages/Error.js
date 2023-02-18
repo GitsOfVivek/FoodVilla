831cc9efd288b4052d80ed00b46fc7e28966107b
GitsOfVivek <singhvivek1999@yahoo.com>
2023-02-18 14:12:14 +0530

    feat | catch error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const Error = () => {
+	const { statusText, status } = useRouteError();
 	return (
-		<div className="wrapper">
+		<div className="error-wrapper">
 			<h1>Oups..!!!</h1>
 			<h3>Somthing went wrong...</h3>
+			<h3>
+				<span>
+					{status} : {statusText}
+				</span>
+			</h3>
 			<Link className="go-to-home" to={'/'}>
 				Go to Home
 			</Link>
